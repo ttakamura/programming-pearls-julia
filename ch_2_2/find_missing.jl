4967062ad913b92508555ca4804db9555366acbb
Tatsuya Takamura <takamura@tonchidot.com>
2015-10-19 22:38:04 +0900

    try ch2.2, find a missing value

--- ch_2_2/find_missing.jl
+++ ch_2_2/find_missing.jl
@@ -1,11 +1,30 @@
-# ファイルには多くて40億の32ビ ット整数がランダムにli1/ん でいて、その中に含まれない32ビ ット整数を1つ見つける のが問題でした。
-# 2^32は4,294,967,296な ので、数字が40億個しかないのなら、その中 に含まれない整数 (「 欠けている整数」とよぶことにします)が必、ずあるはずです。
+#
+# ファイルには多くて40億の32bit整数がランダムに潜んでいて、その中に含まれない32bit整数を1つ見つける問題
+#
 module MissingNum
 
 function generate_test_file(file_name, num_of_nums)
     nums = rand(1:2^32, num_of_nums)
     open(file_name, "w") do io
         write(io, join(nums, "\n"))
+    end
+end
+
+function split_file(in_file_name, left_file_name, right_file_name, partition)
+    num = 0
+    open(in_file_name) do io
+        open(left_file_name, "w") do left_io
+            open(right_file_name, "w") do right_io
+                for l in eachline(io)
+                    num = parse(Int, l)
+                    if num < partition
+                        write(left_io, "$num\n")
+                    else
+                        write(right_io, "$num\n")
+                    end
+                end
+            end
+        end
     end
 end
 
