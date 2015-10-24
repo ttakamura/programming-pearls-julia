912a0c4c12fd79fc05b645047dbdfa4a085d465b
Tatsuya Takamura <takamura@tonchidot.com>
2015-10-25 04:58:00 +0900

    [from now] Sun Oct 25 04:57:59 JST 2015

--- ch_2_2/find_missing.jl
+++ ch_2_2/find_missing.jl
@@ -1,64 +1,61 @@
 #
-# ファイルには多くて40億の32bit整数がランダムに潜んでいて、その中に含まれない32bit整数を1つ見つける問題
+# ファイルには多くて40億程度の32bit整数がランダムに並んでいて、その中に含まれない32bit整数を1つ見つける問題
 #
 module MissingNum
 
-function generate_test_file(file_name, num_of_nums, max_bitnum)
-    nums = rand(1:2^max_bitnum, num_of_nums)
-    open(file_name, "w") do io
-        write(io, join(nums, "\n"))
+function generate_test_file(file, max_bitnum)
+    open(file, "w") do io
+        for num in 1:2^max_bitnum
+            if (num % 97) != 0          # 含まれない数字を適当に
+                write(io, "$num\n")
+            end
+        end
     end
 end
 
-function split_file(in_file_name, left_file_name, right_file_name, partition)
+function split_file(in_file_name, lower_file_name, upper_file_name, partition)
     num         = 0
-    left_count  = 0
-    right_count = 0
+    lower_count = 0
+    upper_count = 0
     open(in_file_name) do io
-        open(left_file_name, "w") do left_io
-            open(right_file_name, "w") do right_io
+        open(lower_file_name, "w") do lower_io
+            open(upper_file_name, "w") do upper_io
                 for l in eachline(io)
                     num = parse(Int, l)
                     if num < partition
-                        left_count += 1
-                        write(left_io, "$num\n")
+                        lower_count += 1
+                        write(lower_io, "$num\n")
                     else
-                        right_count += 1
-                        write(right_io, "$num\n")
+                        upper_count += 1
+                        write(upper_io, "$num\n")
                     end
                 end
             end
         end
     end
-    return left_count, right_count
+    return lower_count, upper_count
 end
 
 function find_missing(in_file_name, min_num, max_num)
-    println("Find missing value, min: $min_num, max: $max_num")
+    println("Finding missing value, min: $min_num, max: $max_num")
 
     base_file_name  = replace(in_file_name, ".dat", "")
-    left_file_name  = "$(base_file_name)_L.dat"
-    right_file_name = "$(base_file_name)_R.dat"
+    lower_file_name = "$(base_file_name)_L.dat"
+    upper_file_name = "$(base_file_name)_R.dat"
     partition_num   = round(Int, (min_num + max_num) / 2)
 
-    left_count, right_count = split_file(in_file_name, left_file_name, right_file_name, partition_num)
+    lower_count, upper_count = split_file(in_file_name, lower_file_name, upper_file_name, partition_num)
 
-    if left_count == 0
+    if upper_count == 0
+        print("Found missing value, $(partition_num)")
+    elseif lower_count == 0
         print("Found missing value, $(partition_num - 1)")
-
-    elseif right_count == 0
-        print("Found missing value, $(partition_num + 1)")
-
-    elseif left_count < right_count
-        println("Go to left, $left_count < $right_count ")
-        find_missing(left_file_name, min_num, partition_num)
-
-    elseif left_count > right_count
-        println("Go to right, $left_count > $right_count ")
-        find_missing(right_file_name, partition_num, max_num)
-
+    elseif lower_count < upper_count
+        println("Go to lower, $lower_count < $upper_count ")
+        find_missing(lower_file_name, min_num, partition_num)
     else
-        error("$left_count == $right_count")
+        println("Go to upper, $lower_count > $upper_count ")
+        find_missing(upper_file_name, partition_num, max_num)
     end
 end
 
